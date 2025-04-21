d93057a4131f9ddcaaaf2c2ffb52f2fd4d0b97fe
Tolo Stamford Otieno <otibomb98@gmail.com>
2025-04-21 18:33:38 +0300

    Delete portScanner directory

--- portScanner/static/js/script.js
+++
@@ -1,6 +0,0 @@
-// Example: Add a loading spinner during scans
-document.querySelector("form").addEventListener("submit", function() {
-    const button = document.querySelector("button");
-    button.innerHTML = "Scanning...";
-    button.disabled = true;
-});
